48164a6537bdddac42f60acb1099dbd2567d4b85
kamal2602 <84529895+kamal2602@users.noreply.github.com>
2026-01-31 05:48:40 +0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
 })
+  resolve: {
+    alias: {
+      xlsx: path.resolve(__dirname, 'node_modules/xlsx/xlsx.mjs'),
+    },
+  },
